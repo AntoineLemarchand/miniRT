78d37a7d755ddd049d96f50a56042430b04f53a0
AntoineLemarchand <antoine.lemarchand05@gmail.com>
2022-06-03 16:15:03 +0200

    little sun

--- srcs/raytracing/trace_shapes.c
+++ srcs/raytracing/trace_shapes.c
@@ -6,7 +6,7 @@
 /*   By: alemarch <alemarch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 14:04:13 by alemarch          #+#    #+#             */
-/*   Updated: 2022/06/03 14:37:25 by alemarch         ###   ########.fr       */
+/*   Updated: 2022/06/03 16:01:50 by alemarch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,7 +63,29 @@ double	get_plane_dist(t_ray *ray, t_plane *plane)
 
 double	get_cylinder_dist(t_ray *ray, t_cylinder *cylinder)
 {
-	(void)ray;
-	(void)cylinder;
-	return (-1);
+	t_vec	center;
+	double	quad[3];
+	double	discr;
+	double	ret[2];
+	double	radius;
+
+	radius = cylinder->diameter / 2;
+	center.x = ray->origin.x - cylinder->position.x;
+	center.y = ray->origin.y - cylinder->position.y;
+	center.z = ray->origin.z - cylinder->position.z;
+	quad[0] = vec_dot_product(&ray->offset, &ray->offset);
+	quad[1] = 2 * vec_dot_product(&center, &ray->offset);
+	quad[2] = vec_dot_product(&center, &center) - radius * radius;
+	discr = quad[1] * quad[1] - 4 * quad[0] * quad[2];
+	if (discr < 0)
+		return (-1);
+	ret[0] = (-quad[1] + sqrtf(discr)) / (2 * quad[0]);
+	ret[1] = (-quad[1] - sqrtf(discr)) / (2 * quad[0]);
+	if (ret[0] < 0)
+	{
+		if (ret[1] < 0)
+			return (-1);
+		return (ret[1]);
+	}
+	return (fmin(ret[0], ret[1]));
 }
