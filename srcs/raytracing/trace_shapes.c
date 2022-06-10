40e5775ce220c6559582a4ea7a734d9a452f4983
AntoineLemarchand <antoine.lemarchand05@gmail.com>
2022-06-10 16:08:39 +0200

    cylinder done i think lmao

--- srcs/raytracing/trace_shapes.c
+++ srcs/raytracing/trace_shapes.c
@@ -6,7 +6,7 @@
 /*   By: alemarch <alemarch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 14:04:13 by alemarch          #+#    #+#             */
-/*   Updated: 2022/06/10 15:31:20 by alemarch         ###   ########.fr       */
+/*   Updated: 2022/06/10 16:01:25 by alemarch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,18 +70,22 @@ double	get_cylinder_dist(t_ray *ray, t_cylinder *cylinder)
 	double	radius;
 
 	radius = cylinder->diameter / 2;
+	t_vec *matrix = build_ray_matrix(&cylinder->orientation);
 	center.x = ray->origin.x - cylinder->position.x;
 	center.y = ray->origin.y - cylinder->position.y;
 	center.z = ray->origin.z - cylinder->position.z;
-	t_vec *matrix = build_ray_matrix(&cylinder->orientation);
+	t_vec newcenter;
+	newcenter.x = center.x * matrix[0].x + center.y * matrix[0].y - center.z * matrix[0].z;
+	newcenter.y = center.x * matrix[1].x + center.y * matrix[1].y - center.z * matrix[1].z;
+	newcenter.z = center.x * matrix[2].x + center.y * matrix[2].y - center.z * matrix[2].z;
 	t_vec newoffset;
-	newoffset.x = ray->offset.x * matrix[0].x + ray->offset.y * matrix[0].y + ray->offset.z * matrix[0].z;
-	newoffset.y = ray->offset.x * matrix[1].x + ray->offset.y * matrix[1].y + ray->offset.z * matrix[1].z;
-	newoffset.z = ray->offset.x * matrix[2].x + ray->offset.y * matrix[2].y + ray->offset.z * matrix[2].z;
+	newoffset.x = ray->offset.x * matrix[0].x + ray->offset.y * matrix[0].y - ray->offset.z * matrix[0].z;
+	newoffset.y = ray->offset.x * matrix[1].x + ray->offset.y * matrix[1].y - ray->offset.z * matrix[1].z;
+	newoffset.z = ray->offset.x * matrix[2].x + ray->offset.y * matrix[2].y - ray->offset.z * matrix[2].z;
 	vec_normalize(&newoffset);
 	quad[0] = powf(newoffset.x, 2.) + powf(newoffset.y, 2.);
-	quad[1] = 2 * center.x * (newoffset.x) + 2 * center.y * (newoffset.y);
-	quad[2] = powf(center.x, 2.) + powf(center.y, 2.) - radius * radius;
+	quad[1] = 2 * newcenter.x * newoffset.x + 2 * newcenter.y * newoffset.y;
+	quad[2] = powf(newcenter.x, 2.) + powf(newcenter.y, 2.) - radius;
 	discr = quad[1] * quad[1] - 4 * quad[0] * quad[2];
 	if (discr < 0)
 		return (-1);
