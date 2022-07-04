f9e73fa1748169572cd42952239028b040914a07
AntoineLemarchand <antoine.lemarchand05@gmail.com>
2022-07-04 11:46:20 +0200

    normed

--- srcs/raytracing/trace_shapes.c
+++ srcs/raytracing/trace_shapes.c
@@ -6,7 +6,7 @@
 /*   By: alemarch <alemarch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 14:04:13 by alemarch          #+#    #+#             */
-/*   Updated: 2022/07/04 11:16:54 by alemarch         ###   ########.fr       */
+/*   Updated: 2022/07/04 11:45:47 by alemarch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,40 +57,48 @@ double	get_plane_dist(t_ray *ray, t_plane *plane)
 	center.x = plane->position.x - ray->origin.x;
 	center.y = plane->position.y - ray->origin.y;
 	center.z = plane->position.z - ray->origin.z;
-	ret = vec_dot_product(&plane->orientation, &center) /
-		vec_dot_product(&plane->orientation, &ray->offset);
+	ret = vec_dot_product(&plane->orientation, &center)
+		/ vec_dot_product(&plane->orientation, &ray->offset);
 	return (ret);
 }
 
-double    get_cylinder_dist(t_ray *ray, t_cylinder *cylinder)
+static double	get_cylinder_discr(t_cylinder *cylinder, t_ray *ray,
+	t_vec *center, double *quad)
 {
-    t_vec    center;
-    t_vec    direction;
-    t_vec    cross_prod[2];
-    double    quad[3];
-    double    discr;
-    double    ret[2];
-    double    radius;
+	t_vec	cross_prod[2];
+	double	radius;
 
-    radius = cylinder->diameter / 2;
-    center.x = ray->origin.x - cylinder->position.x;
-    center.y = ray->origin.y - cylinder->position.y;
-    center.z = ray->origin.z - cylinder->position.z;
-    direction = cylinder->orientation;
-    vec_normalize(&direction);
-    vec_cross_product(&direction, &center, &cross_prod[0]);
-    vec_cross_product(&direction, &ray->offset, &cross_prod[1]);
-    quad[0] = vec_dot_product(&cross_prod[1], &cross_prod[1]);
-    quad[1] = 2 * vec_dot_product(&cross_prod[0], &cross_prod[1]);
-    quad[2] = vec_dot_product(&cross_prod[0], &cross_prod[0]) - radius * radius;
-    discr = quad[1] * quad[1] - 4 * quad[0] * quad[2];
-    if (discr < 0)
-        return (-1);
-    ret[0] = (-quad[1] + sqrtf(discr)) / (2 * quad[0]);
-    ret[1] = (-quad[1] - sqrtf(discr)) / (2 * quad[0]);
-	if (fabs(ret[0]) >= fabs((cylinder->height / 2 - vec_dot_product(&direction, &center)) / vec_dot_product(&direction, &ray->offset)))
+	vec_cross_product(&cylinder->orientation, center, &cross_prod[0]);
+	vec_cross_product(&cylinder->orientation, &ray->offset, &cross_prod[1]);
+	radius = cylinder->diameter / 2;
+	quad[0] = vec_dot_product(&cross_prod[1], &cross_prod[1]);
+	quad[1] = 2 * vec_dot_product(&cross_prod[0], &cross_prod[1]);
+	quad[2] = vec_dot_product(&cross_prod[0], &cross_prod[0]) - radius * radius;
+	return (quad[1] * quad[1] - 4 * quad[0] * quad[2]);
+}
+
+double	get_cylinder_dist(t_ray *ray, t_cylinder *cylinder)
+{
+	double	quad[3];
+	double	discr;
+	double	ret[2];
+	t_vec	center;
+
+	center.x = ray->origin.x - cylinder->position.x;
+	center.y = ray->origin.y - cylinder->position.y;
+	center.z = ray->origin.z - cylinder->position.z;
+	discr = get_cylinder_discr(cylinder, ray, &center, quad);
+	if (discr < 0)
+		return (-1);
+	ret[0] = (-quad[1] + sqrtf(discr)) / (2 * quad[0]);
+	ret[1] = (-quad[1] - sqrtf(discr)) / (2 * quad[0]);
+	if (fabs(ret[0]) >= fabs((cylinder->height / 2
+				- vec_dot_product(&cylinder->orientation, &center))
+			/ vec_dot_product(&cylinder->orientation, &ray->offset)))
 		ret[0] = -1;
-	if (fabs(ret[1]) >= fabs((cylinder->height / 2 - vec_dot_product(&direction, &center)) / vec_dot_product(&direction, &ray->offset)))
+	if (fabs(ret[1]) >= fabs((cylinder->height / 2
+				- vec_dot_product(&cylinder->orientation, &center))
+			/ vec_dot_product(&cylinder->orientation, &ray->offset)))
 		ret[1] = -1;
-    return (return_dist(ret));
+	return (return_dist(ret));
 }
